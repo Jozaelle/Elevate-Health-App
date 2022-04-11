10d7f780a25969ce34c6b6ba15d31d210efd2b13
Brian Rodriguez <brian.epsi@gmail.com>
2022-04-11 17:18:07 -0400

    commiting to help out

---
+++ java/src/main/java/com/techelevator/model/FoodIntake.java
@@ -0,0 +1,13 @@
+package com.techelevator.model;
+
+public class FoodIntake {
+
+    private int food_intake_id;
+    private String food_type;
+    private String serving_size;
+    private String number_of_serving;
+    private String meal_type;
+    //private Date day_of_meal;
+
+
+}
