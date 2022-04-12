9c620b61cb6a0232035c38132c5804cf382a984a
kris jackson <kjackson2319@gmail.com>
2022-04-12 10:59:11 -0600

    commiting work on submission form before lunch

--- vue/src/components/TrackFoodIntake.vue
+++ vue/src/components/TrackFoodIntake.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <form>
+    <form @submit.prevent="createFoodIntake">
       <h2>Food Intake Form</h2>
       <label for="food-type">Type of Food</label>
       <br />
-      <input type="text" id="food-type" placeholder="Type of Food" />
+      <input type="text" id="food-type" placeholder="Type of Food" v-model="foodIntake.food_type" />
       <br />
       <br />
       <label for="serving-size">Serving Size</label>
       <br />
-      <input type="text" id="serving-size" placeholder="Serving Size" />
+      <input type="text" id="serving-size" placeholder="Serving Size" v-model="foodIntake.serving_size" />
       <br />
       <br />
 
@@ -19,13 +19,14 @@
         type="text"
         id="number-of-servings"
         placeholder="Number of Servings"
+        v-model="foodIntake.number_of_servings"
       />
       <br />
       <br />
 
       <label for="meal-type">Meal Breakfast/Lunch/Dinner/Snack</label>
       <br />
-      <select name="meal-type" id="meal-type">
+      <select name="meal-type" id="meal-type" v-model="foodIntake.meal_type">
         <option value="breakfast">Breakfast</option>
         <option value="lunch">Lunch</option>
         <option value="dinner">Dinner</option>
@@ -35,16 +36,43 @@
       <br />
       <label>
         Enter the Date:
-        <input type="date" name="meal-day" />
+        <input type="date" name="meal-day" v-model="foodIntake.day_of_meal" />
       </label>
-
-      <p><button>Submit</button></p>
+      <br />
+      <br />
+      <button type="submit" >Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+
+import foodIntakeService from '../services/FoodIntakeService'
+
+export default {
+
+data() {
+  return {
+    foodIntake: {
+      user_id: "1",
+      food_type: "",
+      serving_size: "",
+      number_of_servings: "",
+      meal_type: "",
+      day_of_meal: "",
+    },
+  }
+},
+
+methods: {
+
+   createFoodIntake() {
+
+    foodIntakeService.createFoodIntake(this.foodIntake)
+  }
+}
+
+};
 </script>
 
 <style>
