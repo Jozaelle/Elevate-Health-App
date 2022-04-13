b20cfea78807141ee4a171845b74ef1b3f743599
kris jackson <kjackson2319@gmail.com>
2022-04-13 15:21:44 -0600

    commiting my one change

---
+++ vue/src/components/HydrationInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <label for="hydration">How Many 8 oz. Glasses of Water Did You Drink Today?</label>
+      <input type="number" id="hydration" name="hydration"
+       min="0" max="100" />
+       <button type="submit"></button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            hydration: {
+
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
