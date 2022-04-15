356dc46227a811129bf45e601bfe584df25b032f
brian <brian.epsi@gmail.com>
2022-04-15 17:44:10 -0400

    fixed food intake css

--- vue/src/views/FoodIntakeView.vue
+++ vue/src/views/FoodIntakeView.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <track-food-intake v-bind:foodIntakeID="parseInt($route.params.foodIntakeID)"/>
-    <br>
-    <br>
-    <br>
-    <hydration-input />
+    <div class="containers">
+      <track-food-intake v-bind:foodIntakeID="parseInt($route.params.foodIntakeID)"/>
+      <br>
+      <br>
+      <hydration-input />
+    </div>
   </div>
 </template>
 
@@ -17,5 +18,18 @@ export default {
 </script>
 
 <style>
+.containers{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
 
+  /* shadow  */
+  background: #dfe5ff;
+  box-shadow: 0 20px 15px 0 rgb(0 0 0 / 40%), 0 6px 20px 0 rgb(0 0 0 / 40%);
+  width: 500px;
+  height: 800px;
+  border-radius: 25px;
+}
 </style>
